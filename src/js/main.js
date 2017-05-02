fb599f7bcb4f52c574c52ba97bd35f1bd17048d0
quiaro <quiaro@gmail.com>
2017-05-02 17:11:51 -0600

    Add side nav menus (filters & listing)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import {helloWorld} from './utils'
 
 helloWorld();
+
+$(document).ready(function() {
+  // Initialize collapse button
+  $(".filter-menu-control").sideNav();
+  $(".listing-menu-control").sideNav();
+
+  $('select').material_select();
+});
