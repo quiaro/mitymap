fd0d1b659236c2fb08470ec479be2715d0698e7c
quiaro <quiaro@gmail.com>
2017-05-04 11:48:08 -0600

    Setup google map in map-app component via binding on init

--- src/components/map-app/index.js
+++ src/components/map-app/index.js
@@ -1,4 +1,38 @@
+const ko = require('knockout');
+const google = require('googleApi');
+const mapStyles = require('./map-styles.json');
 const template = require('./template.html');
+
+// Initialize the map on a specific DOM element in the component via a binding
+// Per: http://knockoutjs.com/documentation/custom-bindings.html
+ko.bindingHandlers.mapSetup = {
+  init: function(element) {
+
+    // Constructor creates a new map - only center and zoom are required.
+    const map = new google.maps.Map(element, {
+      center: {
+        lat: 9.9310584,
+        lng: -84.0753899
+      },
+      zoom: 13,
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: mapStyles
+    });
+
+    var tribeca = {
+      lat: 40.719526,
+      lng: -74.0089934
+    };
+
+    new google.maps.Marker({
+      position: tribeca,
+      map: map,
+      title: 'First Marker!'
+    });
+  }
+};
 
 module.exports = {
   template: template
