128c326762e69fac04da370054d81b24e078812d
quiaro <quiaro@gmail.com>
2017-05-09 00:31:58 -0600

    Add range filter for area

--- src/components/filters-panel/viewModel.js
+++ src/components/filters-panel/viewModel.js
@@ -14,9 +14,14 @@ class viewModel {
     // Properties specific to this component's context
     this.propTypeSelected = ko.observable('0');
     this.isLocationAnywhere = ko.observable(false);
+
     this.priceBounds = ko.observableArray();
     this.minPrice = ko.observable();
     this.maxPrice = ko.observable();
+
+    this.areaBounds = ko.observableArray();
+    this.minArea = ko.observable();
+    this.maxArea = ko.observable();
 
     // Observable that concentrates all property filters. It will delay its
     // change notifications in order to avoid updating the visible properties
@@ -41,13 +46,17 @@ class viewModel {
       // must be updated. Changing the bounds will update the range's min and
       // max values, which will in turn update the propFilters observable.
       const priceBounds = this.properties.getRange(type, 'squareMeterSalePrice');
+      const areaBounds = this.properties.getRange(type, 'area');
       this.priceBounds(priceBounds);
+      this.areaBounds(areaBounds);
     });
 
     // Whenever any of these filters are updated, the propFilters observable will
     // be updated via the updatePropFilters method
     this.minPrice.subscribe(this.updatePropFilters.bind(this));
     this.maxPrice.subscribe(this.updatePropFilters.bind(this));
+    this.minArea.subscribe(this.updatePropFilters.bind(this));
+    this.maxArea.subscribe(this.updatePropFilters.bind(this));
 
     // After the property filters are updated, a new set of visible properties
     // is computed and passed to the handler in charge of updating the
@@ -62,10 +71,13 @@ class viewModel {
     const type = this.propTypeSelected();
     const minPrice = this.minPrice();
     const maxPrice = this.maxPrice();
+    const minArea = this.minArea();
+    const maxArea = this.maxArea();
 
     this.propFilters({
       'type': type,
-      'squareMeterSalePrice': [ minPrice, maxPrice ]
+      'squareMeterSalePrice': [ minPrice, maxPrice ],
+      'area': [ minArea, maxArea ]
     });
   }
 }
