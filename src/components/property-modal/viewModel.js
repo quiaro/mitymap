38ef7a37d7d1628603febe736f151018e5139ff1
quiaro <quiaro@gmail.com>
2017-05-11 17:15:57 -0600

    Add information (mock api data) for property modal

--- src/components/property-modal/viewModel.js
+++ src/components/property-modal/viewModel.js
@@ -1,18 +1,49 @@
 const ko = require('knockout');
+const apiAdapter = require('../../js/apiAdapter');
+const { capitalizeFirstLetter } = require('../../js/utils.js');
 
 class viewModel {
   constructor(params) {
     this.selectedProperty = params.selectedProperty;
     this.actionBtnHandler = params.onActionBtnClick;
 
+    // Properties specific to this component's context
     this.isModalOpen = ko.observable(false);
+    this.isLoading = ko.observable(false);
+    this.property = ko.observable();
 
     /* --- Explicit subscriptions on observables ---*/
     this.selectedProperty.subscribe(property => {
       // Whenever the selected property value changes from null to a property,
       // the modal should open with the selected property information.
       if (property) {
-        console.info('Selected property: ', property);
+        const type = capitalizeFirstLetter(property.type)
+        const title = `${type} in ${property.project}`;
+        const propertyData = {
+          title: title,
+          address: property.address,
+          bedrooms: property.bedrooms,
+          bathrooms: property.bathrooms,
+          area: property.area,
+          salePrice: property.salePrice,
+          extended: false
+        };
+        // Set observable to property data and start loading spinner to get
+        // the rest of the information from API call(s)
+        this.isLoading(true);
+        this.property(propertyData);
+
+        apiAdapter.loadPropertyData(property)
+                  .then((apiData) => {
+                    const data = Object.assign(propertyData, apiData, { extended: true });
+                    // Update observable with property data (merged with API
+                    // data) and stop loading spinner
+                    this.property(data);
+                    this.isLoading(false);
+                  }, () => {
+                    // Stop loading spinner
+                    this.isLoading(false);
+                  });
         this.isModalOpen(true);
       }
     });
--- src/js/apiAdapter/index.js
+++ src/js/apiAdapter/index.js
@@ -2,13 +2,21 @@ const apiKeys = require('./apiKeys');
 
 const loadPropertyData = (property) => {
   return new Promise((resolve, reject) => {
-    if (property) {
-      console.info(apiKeys.facebook);
-      resolve(true);
-    } else {
-      reject('Invalid property');
-    }
-  })
+    console.info(apiKeys.facebook);
+    setTimeout(function() {
+      if (property) {
+        resolve({
+          cover_image: 'https://fb-s-a-a.akamaihd.net/h-ak-fbx/v/t1.0-0/p180x540/22186_1643607169194383_4071659832503055671_n.jpg?oh=e999bac6437f37843fe88722eef07a08&oe=597B1300&__gda__=1504565047_8b006c4e5c5a5619d593496ea120ff63',
+          phone: '25091900',
+          rating_count: 25,
+          overall_rating: 4.5,
+          source: 'facebook'
+        })
+      } else {
+        reject('Invalid property value');
+      }
+    }, 1200);
+  });
 }
 
 module.exports = {
